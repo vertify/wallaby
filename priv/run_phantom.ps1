579d14c28c4f87ab68fd8ea6e7a86e06ebbf4a58
Christian Howe <chowe@thenewoffice.com>
2016-10-25 15:02:18 -0500

    Don't start powershel windows

--- priv/run_phantom.ps1
+++ priv/run_phantom.ps1
@@ -1,6 +1,5 @@
 $a = $MyInvocation.UnboundArguments
 $p = $a[1..($a.length -1)]
-$process = Start-Process $a[0] -ArgumentList "$p" -PassThru
+$process = Start-Process $a[0] -NoNewWindow -ArgumentList "$p" -PassThru
 $waitforcr = Read-Host
-Stop-Process $process.Id
-exit
+Stop-Process $process.Id -Force
